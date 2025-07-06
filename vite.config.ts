e72a8fefc986578f23005fd40a8658fada0c2d91
orji Dominion <orjidominion32@gmail.com>
2025-07-06 13:30:19 +0100

    err sorted

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,11 @@
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// vite.config.ts
+import { defineConfig } from 'vitest/config' 
+import react from '@vitejs/plugin-react'
 
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import { resolve } from 'node:path'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
-    viteReact(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
   },
 })
